2294dc9c5ef3750e4ec6b7d94ed69bffbc6e259a
60346 <60346@etu.he2b.be>
2023-09-17 14:25:24 +0200

    Portfolio : update

--- contact.php
+++ contact.php
@@ -1,12 +1,11 @@
 <?php
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $occasion = $_POST["occasion"];
+    $pack = $_POST["pack"];
+    $message = $_POST["message"];
 
-    $nom = ["nom"];
-    $email = ["email"];
-    $occasion = ["occasion"];
-    $pack = ["pack"];
-    $message = ["message"];
-
-    $destinataire = "focusytbpro@gmail.com"; // Remplacez par votre adresse e-mail
+    $destinataire = "focusytbpro@gmail.com"; // Adresse e-mail du destinataire
     $sujet = "Nouveau message de contact depuis le site";
     $corpsMessage = "Nom et Prénom : $nom\n";
     $corpsMessage .= "Adresse Email : $email\n";
@@ -14,7 +13,15 @@
     $corpsMessage .= "Pack : $pack\n";
     $corpsMessage .= "Message :\n$message";
 
-    mail($destinataire, $sujet, $corpsMessage);
-    header("Location: /Pages/confirmation.html");
+    // Configuration du serveur SMTP
+    $serveurSMTP = "smtp.hostinger.com"; // Remplacez par le serveur SMTP que vous utilisez
+    $portSMTP = 465; // Port SMTP (peut varier selon le serveur)
+    $nomUtilisateurSMTP = "giuseppe.ferrara@visual-dream.be"; // Votre adresse e-mail SMTP
 
+    // Envoi de l'e-mail en utilisant la fonction mail() avec le serveur SMTP
+    if (mail($destinataire, $sujet, $corpsMessage)) {
+        header("Location: /Pages/confirmation.html");
+    } else {
+        echo "Erreur lors de l'envoi de l'e-mail.";
+    }
 ?>
