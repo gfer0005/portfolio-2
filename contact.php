a01d05b48c3d8f4e464eb7d56715f3a3508690cf
60346 <60346@etu.he2b.be>
2023-09-17 17:20:42 +0200

    Portfolio : mail service done

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@
 
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $from = "giuseppe.ferrara@visual-dream.be";
+    $from = "focusytbpro@gmail.com";
     $subject = "Nouveau message de contact depuis le site";
     $headers = "De : " . $from;
     mail($to,$subject, $corpsMessage, $headers);
