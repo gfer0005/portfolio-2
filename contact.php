4d6b66a180e1e57cbd9d0510fbe2e9403809a243
60346 <60346@etu.he2b.be>
2023-09-17 17:11:13 +0200

    Portofolio : updates

--- contact.php
+++ contact.php
@@ -1,27 +1,23 @@
 <?php
     $nom = $_POST["nom"];
-    $email = $_POST["email"];
+    $to = $_POST["email"];
     $occasion = $_POST["occasion"];
     $pack = $_POST["pack"];
     $message = $_POST["message"];
 
-    $destinataire = "focusytbpro@gmail.com"; // Adresse e-mail du destinataire
-    $sujet = "Nouveau message de contact depuis le site";
     $corpsMessage = "Nom et Prénom : $nom\n";
-    $corpsMessage .= "Adresse Email : $email\n";
+    $corpsMessage .= "Adresse Email : $to\n";
     $corpsMessage .= "Occasion : $occasion\n";
     $corpsMessage .= "Pack : $pack\n";
     $corpsMessage .= "Message :\n$message";
 
-    // Configuration du serveur SMTP
-    $serveurSMTP = "smtp.hostinger.com"; // Remplacez par le serveur SMTP que vous utilisez
-    $portSMTP = 465; // Port SMTP (peut varier selon le serveur)
-    $nomUtilisateurSMTP = "giuseppe.ferrara@visual-dream.be"; // Votre adresse e-mail SMTP
 
-    // Envoi de l'e-mail en utilisant la fonction mail() avec le serveur SMTP
-    if (mail($destinataire, $sujet, $corpsMessage)) {
-        header("Location: /Pages/confirmation.html");
-    } else {
-        echo "Erreur lors de l'envoi de l'e-mail.";
-    }
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "giuseppe.ferrara@visual-dream.be";
+    $subject = "Nouveau message de contact depuis le site";
+    $headers = "De : " . $from;
+    mail($to,$subject, $corpsMessage, $headers);
+    mail($from,$subject, $corpsMessage, $headers);
+    header("Location: /Pages/confirmation.html")
 ?>
--- scripts/contact.js
+++ scripts/contact.js
@@ -5,6 +5,8 @@ backbtn.addEventListener("click", () => {
 })
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    // DARK THEME
     const toggleThemeButton = document.getElementById("toggleTheme");
     const backlogo = document.getElementById("back-btn");
 
@@ -38,44 +40,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     loadTheme();
 
-
-
-    const contactForm = document.getElementById("contactForm");
-
-    contactForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Empêche le formulaire de se soumettre normalement
-
-        // Collectez les données du formulaire
-        const nom = document.getElementById("nom").value;
-        const email = document.getElementById("email").value;
-        const occasion = document.getElementById("occasion").value;
-        const pack = document.getElementById("choix").value;
-        const message = document.getElementById("message").value;
-
-        // Créez un objet contenant les données à envoyer au script PHP
-        const formData = new FormData();
-        formData.append("nom", nom);
-        formData.append("email", email);
-        formData.append("occasion", occasion);
-        formData.append("pack", pack);
-        formData.append("message", message);
-
-        // Effectuez une requête AJAX pour envoyer les données au script PHP
-        fetch("contact.php", {
-            method: "POST",
-            body: formData,
-        })
-            .then((response) => {
-                if (response.ok) {
-                    // Redirigez l'utilisateur vers la page de confirmation après l'envoi réussi
-                    window.location.href = "/Pages/confirmation.html";
-                } else {
-                    // Gérez les erreurs ici (par exemple, affichez un message d'erreur à l'utilisateur)
-                    console.error("Erreur lors de l'envoi du formulaire.");
-                }
-            })
-            .catch((error) => {
-                console.error("Erreur lors de l'envoi du formulaire.", error);
-            });
-    });
 });
